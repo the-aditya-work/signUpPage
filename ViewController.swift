bda09eaab43e80bd2899e27be26a7ccd893073e9
Aditya Rai <aditya.22scse1012774@galgotiasuniversity.edu.in>
2025-01-13 17:33:23 +0530

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignUpPage
+//
+//  Created by Aditya Rai on 13/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
